fa9860c06a1d2f11ba33e5950d3459a761f2b1f8
Your Name <e.odesen@gmail.com>
2023-02-07 13:17:16 +0300

    çalışıyor

--- lib/controller/task_controller.dart
+++ lib/controller/task_controller.dart
@@ -1,10 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_todo_app/model/task.dart';
 
 class TaskController extends GetxController {
   final taskList = <Task>[].obs;
 
-  void addTask() {}
+  late TextEditingController textEditingController;
+
+  @override
+  void onInit() {
+    super.onInit();
+    textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    textEditingController.clear();
+  }
+
+  void addTask(String text, DateTime time) {
+    String text = textEditingController.text;
+    DateTime time = DateTime.now();
+    taskList.add(Task(text, time.day.toString()));
+    textEditingController.clear();
+  }
 
   void deleteTask(Task task) {
     taskList.remove(task);
